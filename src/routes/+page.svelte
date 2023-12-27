24190ae07b273413e6bb0e446ca43a8d3edd908e
suryaadi44 <kmsurya.adi44@gmail.com>
2023-12-27 15:48:18 +0800

    add title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
     import ChartSection from "$lib/components/ChartSection.svelte";
 </script>
 
-
 <div class="container mx-auto my-5">
 	<h1 class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
 		Sentiment Analysis Dashboard
